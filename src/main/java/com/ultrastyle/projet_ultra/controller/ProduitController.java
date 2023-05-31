8cf331233f6e87e1121544175ad699e95d861bd9
Aurélien SEURRE <aurelienseurre@orange.fr>
2023-06-01 01:26:58 +0200

    ajout tomcat

--- src/main/java/com/ultrastyle/projet_ultra/controller/ProduitController.java
+++ src/main/java/com/ultrastyle/projet_ultra/controller/ProduitController.java
@@ -24,4 +24,5 @@ public class ProduitController {
         return produitDao.findAll();
     }
 
+
 }
--- src/main/java/com/ultrastyle/projet_ultra/dao/ProduitDao.java
+++ src/main/java/com/ultrastyle/projet_ultra/dao/ProduitDao.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProduitDao extends JpaRepository<Produit, Integer> {
+
+
 }
